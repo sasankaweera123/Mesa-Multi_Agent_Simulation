4884526bd2102fd897d935869cbf98cfda37d7e2
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2022-09-06 23:30:09 +0530

    Created V.1.0.0

---
+++ multiAgentSimulation/agent.py
@@ -0,0 +1,25 @@
+from mesa import Agent
+
+
+class GameAgent(Agent):
+
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.power = 1
+
+    def step(self) -> None:
+        self.move()
+        if self.power > 0:
+            self.power_struggle()
+
+    def move(self) -> None:
+        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def power_struggle(self):
+        cellmate = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmate) > 1:
+            other = self.random.choice(cellmate)
+            other.power -= 1
+            self.power += 1
---
+++ multiAgentSimulation/model.py
@@ -0,0 +1,25 @@
+from mesa import Model
+from agent import GameAgent
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+
+
+class GameModel(Model):
+    def __init__(self, height, width, num_agents):
+        self.n_agents = num_agents
+        self.grid = MultiGrid(height, width, True)
+        self.schedule = RandomActivation(self)
+        self.running = True
+
+        for i in range(self.n_agents):
+            a = GameAgent(i, self)
+            self.schedule.add(a)
+
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+    def step(self):
+        self.schedule.step()
+
+
